33366c4ba2d1fa31c1bb47d4ca41906df94c1172
aburk3 <aburk3@gmail.com>
2019-02-12 19:17:43 -0600

    Added comments to the various methods for clarity

--- src/com/youtube/SimpleDotCom.java
+++ src/com/youtube/SimpleDotCom.java
@@ -7,10 +7,22 @@ public class SimpleDotCom {
     int[] locationCells;
     int numOfHits = 0;
 
+
+    /**
+     * Receives locations of dotCom from test class
+     * Sets the locations equal to locationCells variable
+     */
     public void setLocationCells(int[] locs) {
         locationCells = locs;
     }
 
+
+    /**
+     *
+     * Receives the user's guess as a string and parses it to an int
+     * Loops through the locationCells and increments if it results in a hit
+     *
+     */
     public String checkYourself(String stringGuess) {
 
         int guess = Integer.parseInt(stringGuess);
@@ -27,6 +39,10 @@ public class SimpleDotCom {
             }
         }
 
+
+        /**
+         * If the number of hits equals the total number of cells, it means they have 'killed' the dotCom
+         */
         if (numOfHits == locationCells.length) {
             result = "kill";
         }
