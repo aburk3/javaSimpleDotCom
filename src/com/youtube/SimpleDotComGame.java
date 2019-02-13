a92c7ca12e78fa4fabcdb5d22d50ec5aa125cc18
aburk3 <aburk3@gmail.com>
2019-02-13 05:34:31 -0600

    Added comments to the Game class to more clearly explain functionality

--- src/com/youtube/SimpleDotComGame.java
+++ src/com/youtube/SimpleDotComGame.java
@@ -9,23 +9,35 @@ public class SimpleDotComGame {
 
         SimpleDotCom theDotCom = new SimpleDotCom();
 
+        /**
+         * Create a random number between 0-4.999, cast it to an int, assign to variable
+         */
         int randomNum = (int) (Math.random() * 5);
 
-
+        /**
+         * Set the locations of the 'DotCom Ships'
+         */
         int[] locations = {randomNum, randomNum+1, randomNum+2};
 
         theDotCom.setLocationCells(locations);
 
+
+
         boolean isAlive = true;
 
-
+        /**
+         * Continue to prompt user for input until isAlive = false
+         */
         while(isAlive == true) {
-            String guess = helper.getUserInput("enter a number");
+            String guess = helper.getUserInput("Enter a number");
 
             String result = theDotCom.checkYourself(guess);
 
             numOfGuesses++;
 
+            /**
+             * Result equals 'kill' when the number of hits equals the length of the locations array
+             */
             if (result.equals("kill")) {
                 isAlive = false;
 
